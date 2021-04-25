4f4f683f0b3d35f153e1b880d696cb5642361d9e
Roman Bachmann <git@240.ch>
2021-04-25 19:33:37 +0200

    fix for sed on GNU

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 CLUSTER_NAME=$(kubectl config current-context) && echo "CLUSTER_NAME=$CLUSTER_NAME"
-sed -i '' "s/clustername: .*/clustername: \"$CLUSTER_NAME\"/g" hook-configmap.yml
+sed -i -e "s/clustername: .*/clustername: \"$CLUSTER_NAME\"/g" 02_hook-configmap.yml
 kubectl apply -f .
 kubectl rollout restart daemonset container-lifecycle-hook -n container-lifecycle-hook
